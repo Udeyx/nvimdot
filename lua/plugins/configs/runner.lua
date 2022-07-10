6cf42983656bdfecd59fa59917dba20a6cd5e1af
Udeyx <udeyxx@gmail.com>
2022-07-10 03:09:10 -0400

    adjust code runner

--- lua/plugins/configs/runner.lua
+++ lua/plugins/configs/runner.lua
@@ -8,13 +8,15 @@ end
 local options = {
     startinsert = true,
     filetype = {
-        c = 'cd $dir && gcc $fileName -o $fileNameWithoutExt -g -Wall && $dir/$fileNameWithoutExt',
-        cpp = 'cd $dir && g++ $fileName -o $fileNameWithoutExt -g -Wall && $dir/$fileNameWithoutExt',
+        c = 'cd $dir && gcc $fileName -o $fileNameWithoutExt -g -Wall -lm && $dir/$fileNameWithoutExt',
+        cpp = 'cd $dir && g++ $fileName -o $fileNameWithoutExt -g -Wall -lm && $dir/$fileNameWithoutExt',
         java = 'cd $dir && javac $fileName && java $fileNameWithoutExt',
         python = 'python3 -u',
         typescript = 'deno run',
         lua = 'lua',
         rust = 'cd $dir && rustc $fileName && $dir/$fileNameWithoutExt',
+        latex = 'xelatex',
+        plaintex = 'xelatex',
     },
 }
 
