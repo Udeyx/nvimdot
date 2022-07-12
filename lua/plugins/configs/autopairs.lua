29cc3b1ea7d6672f4713443ee32a11baa2a2162e
Udeyx <udeyxx@gmail.com>
2022-07-12 10:41:37 +0800

    add nvim-surround

--- lua/plugins/configs/autopairs.lua
+++ lua/plugins/configs/autopairs.lua
@@ -5,4 +5,9 @@ if not present then
     return
 end
 
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+local cmp = require 'cmp'
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
 autopairs.setup()
