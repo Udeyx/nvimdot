0b14615a94d136ecb9afe8b4b5bc68a811c2be25
Udeyx <udeyxx@gmail.com>
2022-08-03 17:48:00 +0800

    change options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,82 +1,84 @@
 local opt = vim.opt
 local g = vim.g
--- file options
-opt.backup = false
-opt.swapfile = false
-opt.writebackup = false
-opt.autoread = true
-opt.hidden = true
+local options = {
+    -- file options
+    backup = false,
+    swapfile = false,
+    writebackup = false,
+    autoread = true,
+    hidden = true,
+
+    -- encoding
+    encoding = 'UTF-8',
+    fileencoding = 'utf-8',
+
+    -- line number
+    number = true,
+    relativenumber = true,
+    numberwidth = 2,
+
+    -- enable mouse
+    mouse = 'a',
+
+    -- hight the current line
+    cursorline = true,
+
+    -- always show sign column
+    signcolumn = 'yes',
+
+    -- tab and indent
+    tabstop = 4,
+    softtabstop = 4,
+    shiftround = true,
+    shiftwidth = 4,
+    expandtab = true,
+    autoindent = true,
+    smartindent = true,
+
+    -- searching
+    ignorecase = true,
+    smartcase = true,
+    hlsearch = false,
+    incsearch = true,
+
+    -- hight of cmd
+    cmdheight = 2,
+
+    -- go to previous/next line with h,l,left arrow and right arrow
+    -- when cursor reaches end/beginning of line
+    whichwrap = '<>[]hl',
+
+    -- interval for updating, also used for CursorHold autocommands
+    updatetime = 100,
+
+    -- timeout length of continuous type
+    timeoutlen = 400,
+
+    -- location of split window
+    splitbelow = true,
+    splitright = true,
+
+    -- enable 24-bit color in TUI
+    termguicolors = true,
+
+    -- show mode with plugins
+    showmode = false,
+
+    -- manage clipboard
+    clipboard = 'unnamedplus',
+
+    -- completion type
+    completeopt = { 'menuone', 'noselect' },
+    pumheight = 10,
+    wildmenu = true,
+}
+for k, v in pairs(options) do
+    opt[k] = v
+end
 
 -- use filetype.lua instead of filetype.vim to accelerate nvim startup time
 g.did_load_filetypes = 0
 g.do_filetype_lua = 1
-
--- encoding
-opt.encoding = 'UTF-8'
-opt.fileencoding = 'utf-8'
-
--- line number
-opt.number = true
-opt.relativenumber = true
-opt.numberwidth = 2
-
--- enable mouse
-opt.mouse = 'a'
-
--- hight the current line
-opt.cursorline = true
-
--- always show sign column
-opt.signcolumn = 'yes'
-
--- tab and indent
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftround = true
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.autoindent = true
-opt.smartindent = true
-
--- searching
-opt.ignorecase = true
-opt.smartcase = true
-opt.hlsearch = false
-opt.incsearch = true
-
--- hight of cmd
-opt.cmdheight = 2
-
--- go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line
-opt.whichwrap = '<>[]hl'
-
--- interval for updating, also used for CursorHold autocommands
-opt.updatetime = 100
-
--- timeout length of continuous type
-opt.timeoutlen = 400
-
--- location of split window
-opt.splitbelow = true
-opt.splitright = true
-
--- enable 24-bit color in TUI
-opt.termguicolors = true
-
--- disable nvim intro
-opt.shortmess:append 'sI'
-
--- show mode with plugins
-opt.showmode = false
-
--- manage clipboard
-opt.clipboard = 'unnamedplus'
-
--- completion type
-opt.completeopt = { 'menuone', 'noselect' }
-opt.pumheight = 10
-opt.wildmenu = true
 
 -- disable some builtin vim plugins
 local default_plugins = {
