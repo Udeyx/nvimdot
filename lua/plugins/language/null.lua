ac6285780d44c1635af8ebbed58351bc5a08d07a
彭启阳 <20376007@buaa.edu.cn>
2023-05-26 10:38:23 +0800

    fix autoformat bugs

--- lua/plugins/language/null.lua
+++ lua/plugins/language/null.lua
@@ -16,6 +16,18 @@ local options = {
             filetypes = { 'tex', 'plaintex', 'bib' },
         },
     },
+    -- formatting on save
+    on_attach = function(client, bufnr)
+        local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+        if client.supports_method 'textDocument/formatting' then
+            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = augroup,
+                buffer = bufnr,
+                callback = function() vim.lsp.buf.format { bufnr = bufnr } end,
+            })
+        end
+    end,
 }
 
 null.setup(options)
